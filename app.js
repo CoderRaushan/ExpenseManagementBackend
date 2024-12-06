f2d590a47216db33ad5e107c1cd77200319fc88d
CoderRaushan <raushankumar23082004@gmail.com>
2024-12-06 16:29:57 +0530

    fontend added in backend cor2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser()); // Make sure to add this line
 const corsOptions = {
-  origin: ["https://coderraushan.github.io/ExpenseManagementFrontend"], 
+  origin: ["https://coderraushan.github.io"], 
   credentials: true, 
 };
 app.use(cors(corsOptions));
