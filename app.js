c9250b75cfc057f0254e6238ce3b10448acc716c
CoderRaushan <raushankumar23082004@gmail.com>
2024-12-07 10:08:07 +0530

    netlify added

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser()); // Make sure to add this line
+
 const corsOptions = {
-  origin: "https://rausexpensemanagement.netlify.app"],
+  origin: ["https://rausexpensemanagement.netlify.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
